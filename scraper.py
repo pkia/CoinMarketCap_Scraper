0763dfa5e2a92bb534be0bb690b8c501103b0e92
Evan Dunbar <43516563+pkia@users.noreply.github.com>
2021-05-09 21:42:27 +0100

    reqs

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from datetime import datetime, date
 import time
 from bs4 import BeautifulSoup
@@ -21,7 +22,8 @@ coin_names = []
 scroll_pause_time = 2
 screen_height = driver.execute_script("return window.screen.height;")
 i = 1
-while True:
+coin_exists = False
+while coin_exists is False:
     driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
     i += 1
     time.sleep(scroll_pause_time)
@@ -34,12 +36,18 @@ while True:
             html_soup = BeautifulSoup(driver.page_source, 'html.parser')
             ticker = html_soup.find_all('td', class_ = 'cmc-table__cell cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--sort-by__symbol')
             coin_name = html_soup.find_all('div', class_ ="sc-1kxikfi-0 fjclfm cmc-table__column-name")
-            for x in range(len(ticker)):
-                name = coin_name[x].a.text
-                c_ticker = ticker[x].div.text
+            index = 0
+            while True:
+                name = coin_name[index].a.text
+                c_ticker = ticker[index].div.text
                 if c_ticker in tickers:
-                    break
+                    if coin_name in coin_names:
+                        coin_exists = True
+                        break
                 tickers.append(c_ticker)
                 coin_names.append(name)
+                index += 1
             crypto_df = pd.DataFrame({'Coin_name': coin_names, 'ticker': tickers})
             crypto_df.to_csv('crypto_scrape.csv')
+            
+            
