6008b4823d0063b915d71bd493f4f839513f11dd
Evan Dunbar <43516563+pkia@users.noreply.github.com>
2021-05-10 10:28:24 +0100

    del

--- scraper.py
+++ scraper.py
@@ -21,7 +21,6 @@ time.sleep(2)
 scroll_pause_time = 2
 screen_height = driver.execute_script("return window.screen.height;")
 i = 1
-coin_exists = False
 while True:
     driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
     i += 1
