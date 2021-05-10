c66706b03a43425289321c53d9ea06a02580663b
Evan Dunbar <43516563+pkia@users.noreply.github.com>
2021-05-10 10:13:53 +0100

    finished

--- scraper.py
+++ scraper.py
@@ -12,18 +12,17 @@ options.add_argument('--incognito')
 options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementClickInterceptedException
-now = datetime.now()
+tickers = []
+coin_names = []
 
 driver.get("https://coinmarketcap.com/all/views/all/")
 html_soup = BeautifulSoup
 time.sleep(2)
-tickers = []
-coin_names = []
 scroll_pause_time = 2
 screen_height = driver.execute_script("return window.screen.height;")
 i = 1
 coin_exists = False
-while coin_exists is False:
+while True:
     driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
     i += 1
     time.sleep(scroll_pause_time)
@@ -33,21 +32,21 @@ while coin_exists is False:
         try:
             driver.find_element_by_xpath("/html/body/div/div[1]/div[2]/div/div[1]/div/div[3]/button").click()
         except (ElementClickInterceptedException, TimeoutException, NoSuchElementException):
-            html_soup = BeautifulSoup(driver.page_source, 'html.parser')
-            ticker = html_soup.find_all('td', class_ = 'cmc-table__cell cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--sort-by__symbol')
-            coin_name = html_soup.find_all('div', class_ ="sc-1kxikfi-0 fjclfm cmc-table__column-name")
-            index = 0
-            while True:
-                name = coin_name[index].a.text
-                c_ticker = ticker[index].div.text
-                if c_ticker in tickers:
-                    if coin_name in coin_names:
-                        coin_exists = True
-                        break
-                tickers.append(c_ticker)
-                coin_names.append(name)
-                index += 1
-            crypto_df = pd.DataFrame({'Coin_name': coin_names, 'ticker': tickers})
-            crypto_df.to_csv('crypto_scrape.csv')
-            
+            break
+        
+
+html_soup = BeautifulSoup(driver.page_source, 'html.parser')
+ticker = html_soup.find_all('td', class_ = 'cmc-table__cell cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--sort-by__symbol')
+coin_name = html_soup.find_all('div', class_ ="sc-1kxikfi-0 fjclfm cmc-table__column-name")
+for cur_ticker in ticker:
+    c_ticker = cur_ticker.div.text
+    tickers.append(c_ticker)
+for cur_name in coin_name:
+    name = cur_name.a.text
+    coin_names.append(name)
+    
+    
+crypto_df = pd.DataFrame({'Coin_name': coin_names, 'ticker': tickers})
+crypto_df.to_csv('crypto_scrape.csv')
+driver.Dispose()
             
